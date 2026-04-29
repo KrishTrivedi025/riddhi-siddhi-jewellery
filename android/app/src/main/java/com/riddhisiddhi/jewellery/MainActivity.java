2dd9c3fcbf1d63dd09146c4a3149bab4126c7860
Krushn025 <krrishtrivedi17@gmail.com>
2026-04-29 22:25:22 +0530

    Mobile fixes

--- android/app/src/main/java/com/riddhisiddhi/jewellery/MainActivity.java
+++ android/app/src/main/java/com/riddhisiddhi/jewellery/MainActivity.java
@@ -1,5 +1,14 @@
 package com.riddhisiddhi.jewellery;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Tell Android: WebView draws behind system bars, expose real insets
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+}
