096141f7a731b4625f603683c6366596599df1ea
Krushn025 <krrishtrivedi17@gmail.com>
2026-05-01 21:08:02 +0530

    Fix Android WebView font scaling natively

--- android/app/src/main/java/com/riddhisiddhi/jewellery/MainActivity.java
+++ android/app/src/main/java/com/riddhisiddhi/jewellery/MainActivity.java
@@ -8,4 +8,14 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Prevent Android OS font scaling from breaking the UI
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            android.webkit.WebView webView = this.bridge.getWebView();
+            webView.getSettings().setTextZoom(100);
+        }
+    }
 }
